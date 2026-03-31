06f21d05abbfa0a8d1ca929fa953afe686d27252
ChrisWroot <christopher.wroot@statsperform.com>
2026-03-31 22:21:38 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,3 @@
+import { useState, useMemo, useEffect } from "react";
+import { fetchSheetData } from "./data.js";
+import { STATIC_DATA, PRIZE_OUTCOMES, PLAYERS, TEAM_COLORS, ABBR_FULL, GAME_COLS } from "./constants.js";
