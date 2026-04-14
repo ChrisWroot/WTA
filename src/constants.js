bb2fb5be6b04ae06332795edd286bb9ed1b26ab6
ChrisWroot <chriswroot26@gmail.com>
2026-04-14 12:19:39 +0100

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,29 +1,6 @@
 import STATIC_DATA from './data.json';
 export { STATIC_DATA };
 
-export const GAME_COLS = {
-  "2024": {
-    "Game 1":  { start: 1,  end: 5  },
-    "Game 2":  { start: 6,  end: 10 },
-    "Game 3":  { start: 11, end: 13 },
-    "Game 4":  { start: 14, end: 17 },
-    "Game 5":  { start: 18, end: 24 },
-    "Game 6":  { start: 25, end: 35 },
-  },
-  "2025": {
-    "Game 1":  { start: 36, end: 43 },
-    "Game 2":  { start: 44, end: 45 },
-    "Game 3":  { start: 46, end: 47 },
-    "Game 4":  { start: 48, end: 52 },
-    "Game 5":  { start: 53, end: 54 },
-    "Game 6":  { start: 55, end: 56 },
-    "Game 7":  { start: 57, end: 57 },
-    "Game 8":  { start: 58, end: 62 },
-    "Game 9":  { start: 63, end: 65 },
-    "Game 10": { start: 66, end: 75 },
-  }
-};
-
 export const PLAYERS = ["Ed","Chris W","Franklin","Dan","Patrick","Elliot","Ollie","Chris D","Katy","James","Tom","Jack","Joe","Alex","Emily","Phil","Gareth","Helen","Sam","Keiron"];
 
 export const TEAM_COLORS = {
@@ -52,6 +29,7 @@ export const PRIZE_OUTCOMES = {
     "Game 8": { result: "split", winners: ["Chris D", "Phil"] },
     "Game 9": { result: "win", winners: ["Franklin"] },
     "Game 10": { result: "inprogress", winners: [] }
+    "Game 11": { result: "inprogress", winners: [] },
   }
 };
 
